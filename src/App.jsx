59018256dafe3e684f600ae9c5006457fa226a7c
Faisal <saleemfs27@gmail.com>
2021-08-13 11:21:05 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Test from './Components/Test'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Bruh</h1>
+      <Test />
+    </div>
+  )
+}
+
+export default App
