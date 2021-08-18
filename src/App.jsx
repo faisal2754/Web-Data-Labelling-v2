93bf1193a1ef790393a32f7c4eaf5e0cbd546813
ACohen123 <aharon@hotmail.co.za>
2021-08-18 16:46:57 +0200

    Added Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Nav from './Components/Nav'
 import Navbar from './Components/Navbar'
 import CreateJob from './Pages/CreateJob'
 import Dashboard from './Pages/Dashboard'
+import Login from './Pages/Login'
 import Home from './Pages/Home'
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
@@ -15,6 +16,7 @@ const App = () => {
             <Route path="/" exact component={Home} />
             <Route path="/dashboard" exact component={Dashboard} />
             <Route path="/create-job" exact component={CreateJob} />
+            <Route path="/sign-up" exact component={Login} />
          </Switch>
       </Router>
    )
--- src/Pages/CreateJob.jsx
+++ src/Pages/CreateJob.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import CreateJobForm from '../Components/CreateJobForm'
 
 const CreateJob = () => {
-    return (
-        <div>
-            <h1>Create Job page</h1>
-            <CreateJobForm/>
-        </div>
-    )
+   return (
+      <div>
+         <h1>Create Job page</h1>
+         <CreateJobForm />
+      </div>
+   )
 }
 
 export default CreateJob
