8bd9c372d0b96b130f7684f3ed99cc5541a18868
Sipho SK <sipssman@gmail.com>
2021-08-19 00:18:56 +0200

    Changes to the Dashboard page

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,104 +1,94 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import {
-  Card,
-  Typography,
-  CardContent,
-  CardMedia,
-  CardActionArea,
-  CardActions,
-  Button,
-  Grid,
-  TextField,
+   Card,
+   Typography,
+   InputAdornment,
+   Icon,
+   CardContent,
+   CardMedia,
+   CardActionArea,
+   CardActions,
+   Button,
+   Grid,
+   TextField
 } from '@material-ui/core'
-import SaveIcon from '@material-ui/icons/Save';
+import { Save, Email, People } from '@material-ui/icons'
+import Navbar from '../Components/Navbar'
+import '../Styles/Dashboard.css'
+import Footer from '../Components/Footer'
+import UserDetails from '../Components/UserDetails'
+import UserProfile from '../Components/UserProfile'
 
 const useStyles = makeStyles({
-  profileCard: {
-    maxWidth: 345,
-    marginTop: 20,
-    marginLeft: 200,
-  },
-  media: {
-    height: 140,
-  },
+   profileCard: {
+      maxWidth: 345,
+      marginTop: 20,
+      marginLeft: 200
+   },
+   media: {
+      height: 140
+   },
 
-  form: {
-    maxWidth: '90%',
-    marginTop: 20,
-    height: '150%'
-  },
+   form: {
+      maxWidth: '90%',
+      marginTop: 20,
+      height: '150%'
+   }
 })
 
 export const Dashboard = (props) => {
-  const classes = useStyles()
+   const classNameNamees = useStyles()
 
-  return (
-    <Grid container>
-      <Grid item md={6}>
-        <Card className={classes.profileCard}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="Contemplative Reptile"
+   return (
+      <div>
+         <Navbar />
+         <div>
+            <link
+               href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
+               rel="stylesheet"
             />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button variant="contained" size="large" color="primary" startIcon={<SaveIcon />}>
-              Share
-            </Button>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions>
-        </Card>
-      </Grid>
-
-      {/* Input fields */}
-      <Grid item md={6}>
-        <Card className={classes.form}>
-          <CardContent>
-            <Grid container justifyContent="center" spacing={2}>
-              <Grid item md={6}>
-                <TextField fullWidth id="name" label="Name"/>
-              </Grid>
-              <Grid item md={6}>
-                <TextField fullWidth id="Surname" label="Surname"/>
-              </Grid>
-              <Grid item md={12}>
-                <TextField fullWidth id="name" label="City"/>
-              </Grid>
-              <Grid item md={4}>
-                <TextField fullWidth id="name" label="Country"/>
-              </Grid>
-              <Grid item md={4}>
-                <TextField fullWidth id="name" label="Postal Code"/>
-              </Grid>
-              <Grid item md={4}>
-                <TextField fullWidth id="name" label="Company"/>
-              </Grid>
-            </Grid>
-          </CardContent>
-
-          <CardActions>
-          <Button variant="contained" size="large" color="primary" startIcon={<SaveIcon />}>
-              Update Profile
-            </Button>
-          </CardActions>
-        </Card>
-      </Grid>
-    </Grid>
-  )
+            <div className="main-content">
+               {/* Page content */}
+               <div className="container-fluid mt--7">
+                  <div className="row">
+                     <UserProfile
+                        firstName="John"
+                        lastName="Doe"
+                        age="19"
+                        title="Solution Manager - Creative Tim Officer"
+                        education="University of Computer Science"
+                        about="Ryan — the name taken by Melbourne-raised, Brooklyn-based
+                        Nick Murphy — writes, performs and records all of his own
+                        music."
+                     />
+                     <div className="col-xl-8 order-xl-1">
+                        <div className="card bg-secondary shadow">
+                           <div className="card-header bg-white border-0">
+                              <div className="row align-items-center">
+                                 <div className="col-8">
+                                    <h3 className="mb-0">My account</h3>
+                                 </div>
+                                 <div className="col-4 text-right">
+                                    <a
+                                       href="#!"
+                                       className="btn btn-sm btn-primary"
+                                    >
+                                       Settings
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+                           <UserDetails />
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <Footer />
+         </div>
+      </div>
+   )
 }
 
 export default Dashboard
