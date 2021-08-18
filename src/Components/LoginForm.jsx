16dd2899ab40d8ab32a93fde97057f1771b2988f
ACohen123 <aharon@hotmail.co.za>
2021-08-18 17:50:19 +0200

    Login shell

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import '../Styles/Login.css'
 
-export const Login = () => {
-   return <div></div>
+const Login = () => {
+   return (
+      <div className="login">
+         <img
+            className="login_logo"
+            src="https://logos-download.com/wp-content/uploads/2016/03/Amazon_Logo_2000.png"
+         />
+
+         <div className="login_container">
+            <h1>Login</h1>
+            <form>
+               <h5>Email</h5>
+               <input type="text" />
+
+               <h5>Password</h5>
+               <input type="password" />
+
+               <button>Login</button>
+            </form>
+
+            <p></p>
+         </div>
+      </div>
+   )
 }
 
 export default Login
