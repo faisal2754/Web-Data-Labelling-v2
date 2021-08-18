16229d36c7b7357e3a0d8d84d4263d1369dfc30b
pangsthemans <langiwemwale@gmail.com>
2021-08-18 16:48:54 +0200

    no big changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
-import "./App.css";
-import Nav from "./Components/Nav";
-import Navbar from "./Components/Navbar";
-import CreateJob from "./Pages/CreateJob";
-import Dashboard from "./Pages/Dashboard";
-import Home from "./Pages/Home";
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-=======
 import './App.css'
 import Nav from './Components/Nav'
+import Navbar from './Components/Navbar'
 import CreateJob from './Pages/CreateJob'
 import Dashboard from './Pages/Dashboard'
 import Home from './Pages/Home'
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
->>>>>>> 45599f6060dcc15184d177b33b4d9fc3cec4217d
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
+      <Navbar/>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/dashboard" exact component={Dashboard} />
--- src/Components/CreateJobForm.jsx
+++ src/Components/CreateJobForm.jsx
@@ -1,36 +1,48 @@
-import { useState } from "react";
-import React from "react";
+import { useState } from 'react'
+import React from 'react'
 
 const CreateJob = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+   const [title, setTitle] = useState('')
+   const [description, setDescription] = useState('')
+   const [file, setImagePreview] = useState('')
+   return (
+      <div>
+         <form>
+            <div className="textField">
+               <label>Title</label>
+               <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => {
+                     setTitle(e.target.value)
+                  }}
+               />
+            </div>
+            <div className="textField">
+               <label>Description</label>
+               <textarea
+                  value={description}
+                  onChange={(e) => {
+                     setDescription(e.target.value)
+                  }}
+               ></textarea>
+            </div>
+            <div className='imageSection'>
+               <img src={file} alt="image" />
+               <input
+                  id="imageInput"
+                  type="file"
+                  accept="image/*"
+                  multiple
+                  onchange={(e) => {
+                     URL.createObjectURL(e.target.files[0])
+                  }}
+               />
+               <label htmlFor="imageInput" className="imageUpload"></label>
+            </div>
+         </form>
+      </div>
+   )
+}
 
-  return (
-    <div>
-      <form>
-        <div>
-          <label>title</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label>Description</label>
-          <textarea
-            value={description}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-          ></textarea>
-        </div>
-        <div></div>
-      </form>
-    </div>
-  );
-};
-
-export default CreateJob;
+export default CreateJob
