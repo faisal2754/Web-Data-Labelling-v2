d0b64f77a8ad7f9cac3e8c636b410fa5f1a2711b
ACohen123 <aharon@hotmail.co.za>
2021-08-19 22:58:14 +0200

    login & register navigation and style

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import '../Styles/Register.css'
+import { useHistory } from 'react-router-dom'
+import Checkbox from '@material-ui/core/Checkbox'
 
 const Register = () => {
+   const history = useHistory()
+
+   const [checked, setChecked] = React.useState(true)
+
+   const handleChange = (event) => {
+      setChecked(event.target.checked)
+   }
+
    return (
       <div className="register">
          <div className="register_container">
@@ -18,13 +28,28 @@ const Register = () => {
                   id="conf_pword"
                   placeholder=" Confirm Password*"
                />
-
-               <button className="register_registerButton">Register</button>
             </form>
 
-            <p>Already have an account? </p>
+            <button
+               onClick={() => {
+                  history.push('/')
+               }}
+               className="register_registerButton"
+            >
+               Register
+            </button>
 
-            <button className="register_loginButton">Login</button>
+            <p>By registering, you accept our Terms & Conditions</p>
+
+            <button
+               onClick={() => {
+                  history.push('/login')
+               }}
+               className="register_loginButton"
+            >
+               Login
+            </button>
+            <p>Have an account? Login </p>
          </div>
       </div>
    )
