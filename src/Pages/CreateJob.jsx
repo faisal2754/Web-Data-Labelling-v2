ef6538c8c67fa050528a0e254d829878f136231d
pangsthemans <langiwemwale@gmail.com>
2021-08-19 21:08:29 +0200

    added nav and foot to createjob page

--- src/Pages/CreateJob.jsx
+++ src/Pages/CreateJob.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CreateJobForm from '../Components/CreateJobForm'
-
+import Footer from '../Components/Footer'
+import Navbar from '../Components/Navbar'
 const CreateJob = () => {
    return (
       <div>
+          <Navbar/>
          <h1>Create Job page</h1>
          <CreateJobForm />
+         <Footer/>
       </div>
    )
 }
