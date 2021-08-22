90d22b53892df05998489404ec9566370c74a412
Ioanni-Balassis <1848800@students.wits.ac.za>
2021-08-22 15:42:42 +0200

    ViewJob Page Stylings
    Added:
    Navbar
    Footer
    DropDown Filter Button
    Changed Aesthetic to match rest of webiste
    
    Still To Add:
    Modal

--- src/Pages/ViewJob.jsx
+++ src/Pages/ViewJob.jsx
@@ -1,52 +1,84 @@
 import React, { useState } from 'react'
 import '../Styles/ViewJob.css'
-import Button from '@material-ui/core/Button'
+import {Button} from '../Components/Button'
 import CardItem from '../Components/CardItem'
 import '../Styles/Cards.css'
 import NavbarOther from '../Components/NavbarOther'
+import { Menu, MenuItem } from '@material-ui/core'
+import '../App.css'
+import Footer from '../Components/Footer'
+
 
 function ViewJob() {
+   const[anchorElement,setAnchorElement] = useState(null)
+   const  handleOpenMenu = e => {
+      setAnchorElement(e.currentTarget);
+   }
+   const  handleCloseMenu = e => {
+      setAnchorElement(null)
+   }
+
    return (
       <div className="viewJob">
          <NavbarOther />
 
          <div className="viewJob__header">
-            <img
-               className="viewJob__banner"
-               src="images\ViewJobsBanner.png"
-               alt=""
-            />
+            <video src="./videos/gradient.mp4" autoPlay loop muted/>
 
-            <div className="viewJob__headerWriting">
-               <b>Find The Jobs That Interest You!</b>
-            </div>
+           <h1>Find A Job That Interests You</h1>
          </div>
 
          <div className="viewJob__filters">
-            <b>Filter Your Jobs By: </b>
-            <div className="viewJob__filterButtons">
-               <Button variant="contained">
-                  <b>Name Asc</b>
-               </Button>
-               <Button variant="contained">
-                  <b>Name Desc</b>
-               </Button>
-               <Button variant="contained">
-                  <b>Credits Asc</b>
-               </Button>
-               <Button variant="contained">
-                  <b>Credits Desc</b>
-               </Button>
-               <Button variant="contained">
-                  <b># Images Asc</b>
-               </Button>
-               <Button variant="contained">
-                  <b># Images Desc</b>
-               </Button>
-            </div>
+            <h1><b>Filter Your Jobs By: </b></h1>
+            <Button 
+            onClick={handleOpenMenu}
+            aria-controls='filter__menu'
+            >Filters</Button>
+            
+            <Menu 
+            id="filter__menu"
+            anchorElement={anchorElement} 
+            open={Boolean(anchorElement)}
+            onClose={handleCloseMenu}
+            >
+               <MenuItem>Latest</MenuItem>
+               <MenuItem>Name ASC</MenuItem>
+               <MenuItem>Name DESC</MenuItem>
+               <MenuItem>Credits ASC</MenuItem>
+               <MenuItem>Credits DESC</MenuItem>
+               <MenuItem># Photos ASC</MenuItem>
+               <MenuItem># Photos DESC</MenuItem>
+            </Menu>
+
          </div>
 
          <div className="viewJob__container">
+            <div className="viewJob__row">
+               <CardItem
+                  src="./images/purple_gradient.jpg"
+                  text="Job Name"
+                  credits="1 credit"
+               />
+
+               <CardItem
+                  src="./images/purple_gradient.jpg"
+                  text="Job Name"
+                  credits="1 credit"
+               />
+
+               <CardItem
+                  src="./images/purple_gradient.jpg"
+                  text="Job Name"
+                  credits="1 credit"
+               />
+
+               <CardItem
+                  src="./images/purple_gradient.jpg"
+                  text="Job Name"
+                  credits="10 credit"
+               />
+            </div>
+            
             <div className="viewJob__row">
                <CardItem
                   src="./images/purple_gradient.jpg"
@@ -73,6 +105,7 @@ function ViewJob() {
                />
             </div>
          </div>
+         <Footer />
       </div>
    )
 }
