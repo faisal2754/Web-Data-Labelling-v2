24f3ecb75a5190ef38b8d30330a270d054b1ee0b
ACohen123 <aharon@hotmail.co.za>
2021-08-22 15:37:15 +0200

    restyled login and register

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -1,55 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/Register.css'
+import { Link } from 'react-router-dom'
 import { useHistory } from 'react-router-dom'
 import Checkbox from '@material-ui/core/Checkbox'
 
 const Register = () => {
-   const history = useHistory()
-
+   const [username, setUsername] = useState('')
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
    const [checked, setChecked] = React.useState(true)
+
+   const signIn = (e) => {
+      e.preventDefault()
+      //some backend functionality
+   }
+
+   const register = (e) => {
+      e.preventDefault()
+      //some backend functionality
+   }
+   const history = useHistory()
 
    const handleChange = (event) => {
       setChecked(event.target.checked)
    }
 
    return (
-      <div className="register">
-         <div className="register_container">
-            <h1>Register</h1>
-            <form>
-               <input type="text" id="username" placeholder="Username*" />
+      <div className="register_container">
+         <div class="forms-container">
+            <form class="register-form">
+               <Link to="/">
+                  <img className="login_logo" src="./images/login_logo.png" />
+               </Link>
+               <h2 class="title">Register</h2>
+               <div class="input-field">
+                  <i class="fas fa-user icon"></i>
+                  <input
+                     type="text"
+                     value={username}
+                     onChange={(e) => setUsername(e.target.value)}
+                     id="username"
+                     placeholder="Username"
+                  />
+               </div>
+               <div class="input-field">
+                  <i class="fas fa-envelope icon"></i>
+                  <input
+                     type="text"
+                     value={email}
+                     onChange={(e) => setEmail(e.target.value)}
+                     id="email"
+                     placeholder="Email"
+                  />
+               </div>
+               <div class="input-field">
+                  <i class="fas fa-lock icon"></i>
+                  <input
+                     type="password"
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                     id="pword"
+                     placeholder="Password"
+                  />
+               </div>
+               <div class="input-field">
+                  <i class="fas fa-lock icon"></i>
+                  <input
+                     type="password"
+                     value={checked}
+                     onChange={(e) => setChecked(e.target.value)}
+                     id="conf_pword"
+                     placeholder=" Confirm Password"
+                  />
+               </div>
+               <button
+                  type="submit"
+                  onClick={() => {
+                     history.push('/')
+                  }}
+                  className="register_registerButton"
+               >
+                  SIGN UP
+               </button>
 
-               <input type="text" id="email" placeholder="Email*" />
+               <p class="social-text">Or sign up with</p>
+               <div class="social-media">
+                  <a href="#" class="social-icon">
+                     <i class="fab fa-facebook-f"></i>
+                  </a>
+                  <a href="#" class="social-icon">
+                     <i class="fab fa-twitter"></i>
+                  </a>
+                  <a href="#" class="social-icon">
+                     <i class="fab fa-google"></i>
+                  </a>
+               </div>
+               <p class="other-text">
+                  Already a member?{' '}
+                  <a id="hover" class="underlineHover" href="/login">
+                     Login now
+                  </a>
+               </p>
 
-               <input type="text" id="pword" placeholder="Password*" />
-
-               <input
-                  type="text"
-                  id="conf_pword"
-                  placeholder=" Confirm Password*"
-               />
+               <p>By registering, you accept our Terms & Conditions</p>
             </form>
-
-            <button
-               onClick={() => {
-                  history.push('/')
-               }}
-               className="register_registerButton"
-            >
-               Register
-            </button>
-
-            <p>By registering, you accept our Terms & Conditions</p>
-
-            <button
-               onClick={() => {
-                  history.push('/login')
-               }}
-               className="register_loginButton"
-            >
-               Login
-            </button>
-            <p>Have an account? Login </p>
          </div>
       </div>
    )
