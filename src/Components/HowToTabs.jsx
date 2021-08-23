4ec885cf8dc5920b876415f0e6e10232d4c8a776
Ghulame Mujtaba Arbi <mujtaba.rbee091@gmail.com>
2021-08-23 08:01:06 +0200

    added tabs to how it works page

---
+++ src/Components/HowToTabs.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import { Button } from './Button'
+import { Link } from 'react-router-dom'
+import '../Styles/tab.css'
+
+function HowToTabs() {
+   const [click, setClick] = useState(false)
+   const [button, setButton] = useState(true)
+
+   const handleClick = () => setClick(!click)
+   const closeMobileMenu = () => setClick(false)
+
+   const showButton = () => {
+      if (window.innerWidth <= 960) {
+         setButton(false)
+      } else {
+         setButton(true)
+      }
+   }
+
+   useEffect(() => {
+      showButton()
+   }, [])
+
+   window.addEventListener('resize', showButton)
+
+   return (
+      <nav className="navbar-other">
+         <div className="navbar-container">
+            <div className="menu-icon" onClick={handleClick}>
+               <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+               <li className="nav-item">
+                  <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                     How to Submit Jobs
+                  </Link>
+               </li>
+               <li className="nav-item">
+                  <Link
+                     to="/dashboard"
+                     className="nav-links"
+                     onClick={closeMobileMenu}
+                  >
+                     How to Find Jobs
+                  </Link>
+               </li>
+            </ul>
+         </div>
+      </nav>
+   )
+}
+
+export default HowToTabs
--- src/Pages/HowTo.jsx
+++ src/Pages/HowTo.jsx
@@ -4,12 +4,13 @@ import HowToSection from '../Components/HowToSection'
 import Footer from '../Components/Footer'
 import PostJobCards from '../Components/PostJobCards'
 import NavbarOther from '../Components/NavbarOther'
+import HowTotabs from '../Components/HowToTabs'
 
 function HowTo() {
    return (
       <div>
          <NavbarOther />
-
+         <HowTotabs />
          <HowToSection />
          <PostJobCards />
          <Footer />
