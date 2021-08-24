36079b1f459a5240d118aa2c8f8e96ae4a78a862
Ioanni-Balassis <1848800@students.wits.ac.za>
2021-08-24 11:43:01 +0200

    Modal Fixed
    Fixed Modal, Positioning needs to change

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,45 +1,124 @@
-import React from 'react'
-import '../Styles/Modal.css'
+import React, { useRef, useEffect, useCallback } from 'react'
+import { useSpring, animated } from 'react-spring'
 import styled from 'styled-components'
+import { MdClose } from 'react-icons/md'
 
-function Modal({ setOpenModal }, props) {
+const Background = styled.div`
+   z-index: 1;
+   width: 100%;
+   height: 100%;
+   background: rgba(0, 0, 0, 0.8);
+   position: fixed;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+`
+
+const ModalWrapper = styled.div`
+   width: 800px;
+   height: 500px;
+   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+   background: #fff;
+   color: #000;
+   display: grid;
+   grid-template-columns: 1fr 1fr;
+   position: relative;
+   z-index: 10;
+   border-radius: 10px;
+`
+
+const ModalImg = styled.img`
+   width: 100%;
+   height: 100%;
+   border-radius: 10px 0 0 10px;
+   background: #000;
+`
+
+const ModalContent = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   line-height: 1.8;
+   color: #141414;
+   p {
+      margin-bottom: 1rem;
+   }
+   button {
+      padding: 10px 24px;
+      background: #141414;
+      color: #fff;
+      border: none;
+   }
+`
+
+const CloseModalButton = styled(MdClose)`
+   cursor: pointer;
+   position: absolute;
+   top: 20px;
+   right: 20px;
+   width: 32px;
+   height: 32px;
+   padding: 0;
+   z-index: 10;
+`
+
+export const Modal = ({ showModal, setShowModal }) => {
+   const modalRef = useRef()
+
+   const animation = useSpring({
+      config: {
+         duration: 250
+      },
+      opacity: showModal ? 1 : 0,
+      transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+   })
+
+   const closeModal = (e) => {
+      if (modalRef.current === e.target) {
+         setShowModal(false)
+      }
+   }
+
+   const keyPress = useCallback(
+      (e) => {
+         if (e.key === 'Escape' && showModal) {
+            setShowModal(false)
+            console.log('I pressed')
+         }
+      },
+      [setShowModal, showModal]
+   )
+
+   useEffect(() => {
+      document.addEventListener('keydown', keyPress)
+      return () => document.removeEventListener('keydown', keyPress)
+   }, [keyPress])
+
    return (
-      <div className="modal__background">
-         <div className="modal__container">
-            <div className="modal__close">
-               <button
-                  onClick={() => {
-                     setOpenModal(false)
-                  }}
-               >
-                  {' '}
-                  X{' '}
-               </button>
-            </div>
-            <div className="modal__jobName">Job Name{/* {props.name} */}</div>
-            <div className="modal__wrapper">
-               <img
-                  className="modal__image"
-                  alt="Travel"
-                  src="./images/purple_gradient.jpg"
-                  //   src={props.src}
-               />
-            </div>
-            <div className="modal__info">
-               <div className="modal__description">
-                  This is the description
-                  {/* {props.description} */}
-               </div>
-               <div className="modal__credits">
-                  <b>$</b> 100
-                  {/* {props.credits} */}
-               </div>
-               <div className="modal__uploader">
-                  Uploader Email{props.uploader}
-               </div>
-            </div>
-         </div>
-      </div>
+      <>
+         {showModal ? (
+            <Background onClick={closeModal} ref={modalRef}>
+               <animated.div style={animation}>
+                  <ModalWrapper showModal={showModal}>
+                     <ModalImg
+                        src="./images/purple_gradient.jpg"
+                        alt="camera"
+                     />
+                     <ModalContent>
+                        <h1>Are you ready?</h1>
+                        <p>Get exclusive access to our next launch.</p>
+                        <button>Join Now</button>
+                     </ModalContent>
+                     <CloseModalButton
+                        aria-label="Close modal"
+                        onClick={() => setShowModal((prev) => !prev)}
+                     />
+                  </ModalWrapper>
+               </animated.div>
+            </Background>
+         ) : null}
+      </>
    )
 }
 
