97ac9603a07f607a6334a5f208768ba6475c6568
jononunes <jnunes038@gmail.com>
2021-08-24 10:28:25 +0200

    Commented out css clash

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,38 +1,34 @@
 import React from 'react'
 import '../Styles/Modal.css'
+import styled from 'styled-components'
 
-function Modal(props , {setOpenModal}) {
-    return (
-        <div className="modal__background">
-            <div className="modal__container">
-                <div className="modal__close">
-                <button onClick={() => {setOpenModal(false);}}> X </button>
-                </div>
-                <div className="modal__jobName">
-                    {props.name}
-                </div>
-                <div className="modal__wrapper">
-                <img
-                  className="modal__image"
-                  alt="Travel"
-                  src={props.src}
-               />  
-               </div>
-                <div className="modal__description">
-                    {props.description}
-                </div>  
-                <div className="modal__credits">
-                 <b>$</b>
-                 {props.credits}
-                </div>  
-                <div className="modal__uploader">
-                    {props.uploader}
-                </div>  
-    
-                
+function Modal(props, { setOpenModal }) {
+   return (
+      <div className="modal__background">
+         <div className="modal__container">
+            <div className="modal__close">
+               <button
+                  onClick={() => {
+                     setOpenModal(false)
+                  }}
+               >
+                  {' '}
+                  X{' '}
+               </button>
             </div>
-        </div>
-    )
+            <div className="modal__jobName">{props.name}</div>
+            <div className="modal__wrapper">
+               <img className="modal__image" alt="Travel" src={props.src} />
+            </div>
+            <div className="modal__description">{props.description}</div>
+            <div className="modal__credits">
+               <b>$</b>
+               {props.credits}
+            </div>
+            <div className="modal__uploader">{props.uploader}</div>
+         </div>
+      </div>
+   )
 }
 
 export default Modal
