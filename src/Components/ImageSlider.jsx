214d7e782209bd61b7200f5447c295120b47938a
jononunes <jnunes038@gmail.com>
2021-08-25 16:14:28 +0200

    Fixed testimonials, changed label job page

--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -20,13 +20,15 @@ const ImageSlider = ({ slides }) => {
    }
 
    return (
-      <section className="slider">
+      <section className="label-slider">
          <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
          <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
          {SliderData.map((slide, index) => {
             return (
                <div
-                  className={index === current ? 'slide active' : 'slide'}
+                  className={
+                     index === current ? 'label-slide active' : 'label-slide'
+                  }
                   key={index}
                >
                   {index === current && (
