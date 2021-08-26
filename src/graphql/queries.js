80c538a7f368e1197f489052964cadc08c68d673
Ioanni-Balassis <1848800@students.wits.ac.za>
2021-08-26 15:56:17 +0200

    Updated queries.js for ViewJob
    Updated Queires to pull jobs

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,15 +1,28 @@
-
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client'
 
 const GET_USERS = gql`
-    query Users{
-        users{
-            balance
-            avatar
-        }
-    }
+   query Users {
+      users {
+         balance
+         avatar
+      }
+   }
 `
 
+const GET_JOBS = gql`
+   query ViewJobs {
+      viewJobs {
+         created_at
+         credits
+         description
+         job_id
+         preview_images
+         title
+         job_owner {
+            username
+         }
+      }
+   }
+`
 
-
-export { GET_USERS }
+export { GET_USERS, GET_JOBS }
