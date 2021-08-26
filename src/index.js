44afd1cd92d0b9b83225c58fa23f7a45ec234f8d
Ioanni-Balassis <1848800@students.wits.ac.za>
2021-08-26 16:09:03 +0200

    ViewJob Query Working
    ViewJob Query Works
    changed the index.js to the correct one

--- src/index.js
+++ src/index.js
@@ -3,25 +3,24 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App.jsx'
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
+   ApolloClient,
+   InMemoryCache,
+   ApolloProvider,
+   useQuery,
+   gql
+} from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'https://data-labelling-server-prod.herokuapp.com/graphql',
-  cache: new InMemoryCache(),
-  credentials: "include",
-  
-});
+   uri: 'https://data-labelling-server.herokuapp.com/graphql',
+   cache: new InMemoryCache(),
+   credentials: 'include'
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <ApolloProvider client={client}>
+         <App />
+      </ApolloProvider>
+   </React.StrictMode>,
+   document.getElementById('root')
 )
