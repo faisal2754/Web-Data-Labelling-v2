9428f89a02f455ddc4bf4a3d91641896b05cf79a
pangsthemans <langiwemwale@gmail.com>
2021-09-06 15:29:50 +0200

    attempt at fixing file upload failed

--- src/Components/CreateJobForm.jsx
+++ src/Components/CreateJobForm.jsx
@@ -3,26 +3,26 @@ import React from 'react'
 import '../Styles/CreateJob.css'
 import TextField from '@material-ui/core/TextField'
 import { nanoid } from 'nanoid'
-// import ImageUploading from 'react-images-uploading'
+import ImageUploading from 'react-images-uploading'
 import { useMutation } from '@apollo/client'
 import { CREATE_JOB } from '../graphql/mutations'
 import { Redirect } from 'react-router-dom'
-import { typeFromAST } from 'graphql'
 import { toast } from 'react-toastify'
-
+const FormData = require('form-data')
+const axios = require('axios').default
 
 const CreateJob = () => {
    const [labels, setLabels] = useState([])
    const [createJob, { loading, error, data }] = useMutation(CREATE_JOB)
    const [currentTotal, setCurrentTotal] = useState(0)
-   const [images, setImages] = useState([])
+   // const [images, setImages] = useState([])
    // const onChange = (imageList, addUpdateIndex) => {
    //    // data for submit
    //    // console.log(imageList, addUpdateIndex)
    //    setImages(imageList)
    // }
    const showError = () => {
-      toast.error("An error occured")
+      toast.error('An error occured')
       toast.clearWaitingQueue()
    }
 
@@ -36,12 +36,58 @@ const CreateJob = () => {
    if (error) {
       console.log(error)
    }
-   const handlefiles = (e) => {
-      setImages()
-      let myfiles = e.target.files
+   // let myfiles
+   // const handlefiles = (e) => {
+   //    myfiles = e.target.files
+   // }
 
+   const createFormDataJob = async () => {
+      const form = new FormData()
+      form.append(
+         'operations',
+         JSON.stringify({
+            query: 'mutation ($files: [Upload], $title: String!, $description: String!, $credits: Int!, $num_partitions: Int!, $labels: [String]!){\n  createJob (files: $files, title: $title, description: $description, credits: $credits, num_partitions: $num_partitions, labels: $labels){\n    job_id\n  }\n}',
+            variables: {
+               title: document.querySelector('#title').value,
+               description: document.querySelector('#description').value,
+               labels: labels.map((label) => label.label),
+               // images: images,
+               // images: images.map((image) => image.file),
+               credits: currentTotal,
+               numLabellers: parseInt(
+                  document.querySelector('#numLabellers').value
+               ),
+               numPartitions: parseInt(
+                  document.querySelector('#imgPerSection').value
+               )
+            }
+         })
+      )
+      // var images = document.querySelector('#testimageup')
+      // console.log(images)
+      form.append(
+         'map',
+         JSON.stringify(
+            // images.map((image) => {
+            //    const originalimgname = image.file.name
+            //    return { originalimgname: ['variables.files.originalimgname'] }
+            // })
+         )
+      )
+      // images.forEach((img) => {
+      //    const originalimgname = img.file.name
+      //    form.append(originalimgname, img.data_url)
+      // })
+
+      // const headers = form.getHeaders()
+      console.log(form)
+      const res = await axios.post(
+         'https://data-labelling-server.herokuapp.com/graphql',
+         form
+      )
+      console.log(res)
    }
-
+ 
    const Calculate = (e) => {
       e.preventDefault()
       let currentCredits = document.querySelector('#credits').value
@@ -58,8 +104,9 @@ const CreateJob = () => {
       <div className="createJob_page">
          <form
             encType="multipart/form-data"
-            onSubmit={(e) => {
+            onSubmit={async (e) => {
                e.preventDefault()
+               // await createFormDataJob()
                // console.log(e.target.files)
                const dataForSubmit = {
                   jobTitle: document.querySelector('#title').value,
@@ -77,14 +124,14 @@ const CreateJob = () => {
                }
 
                console.log(dataForSubmit)
-               createJob({
+               await createJob({
                   variables: {
                      title: dataForSubmit.jobTitle,
                      description: dataForSubmit.jobDescription,
                      credits: dataForSubmit.totalCreditsCost,
                      labels: dataForSubmit.labels,
                      num_partitions: dataForSubmit.numPartitions,
-                     files: dataForSubmit.images
+                     files: dataForSubmit.images[0]
                   }
                }).catch((error) => showError())
             }}
@@ -206,13 +253,13 @@ const CreateJob = () => {
                      id="testimageup"
                      type="file"
                      multiple
-                     onChange={handlefiles}
+                     // onChange={handlefiles}
                   />
                   {/* <ImageUploading
                      multiple
                      value={images}
                      onChange={onChange}
-                     maxNumber={maxNumber}
+                     
                      dataURLKey="data_url"
                   >
                      {({
@@ -258,25 +305,25 @@ const CreateJob = () => {
                                        width="200"
                                     />
                                     {/* <div className="image-item__btn-wrapper">
-                                    <button
-                                       className="btn-hover"
-                                       onClick={(e) => {
-                                          onImageUpdate(index)
-                                          e.preventDefault()
-                                       }}
-                                    >
-                                       Update
-                                    </button>
-                                    <button
-                                       className="btn-hover"
-                                       onClick={(e) => {
-                                          onImageRemove(index)
-                                          e.preventDefault()
-                                       }}
-                                    >
-                                       Remove
-                                    </button>
-                                 </div> 
+                                       <button
+                                          className="btn-hover"
+                                          onClick={(e) => {
+                                             onImageUpdate(index)
+                                             e.preventDefault()
+                                          }}
+                                       >
+                                          Update
+                                       </button>
+                                       <button
+                                          className="btn-hover"
+                                          onClick={(e) => {
+                                             onImageRemove(index)
+                                             e.preventDefault()
+                                          }}
+                                       >
+                                          Remove
+                                       </button>
+                                    </div> 
                                  </div>
                               ))}
                            </div>
@@ -302,7 +349,7 @@ const CreateJob = () => {
                   />
                   <TextField
                      id="imgPerSection"
-                     label="Number of Images per section"
+                     label="Number of Partitions"
                      type="number"
                      // onChange={Calculate}
                      variant="outlined"
