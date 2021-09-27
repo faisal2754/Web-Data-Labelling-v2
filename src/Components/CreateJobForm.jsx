d9028b815b36758ca7aa45ae7fadd0a085a45f3c
Langiwe William Mwale <langiwemwale@gmail.com>
2021-09-27 09:27:55 +0200

    success message on create job , cleaned old code

--- src/Components/CreateJobForm.jsx
+++ src/Components/CreateJobForm.jsx
@@ -4,47 +4,25 @@ import '../Styles/CreateJob.css'
 import TextField from '@material-ui/core/TextField'
 import { nanoid } from 'nanoid'
 import ImageUploading from 'react-images-uploading'
-import { useMutation } from '@apollo/client'
-import { CREATE_JOB } from '../graphql/mutations'
 import { Redirect } from 'react-router-dom'
 import Cookies from 'js-cookie'
 import { toast } from 'react-toastify'
 import FormData from 'form-data'
 import axios from 'axios'
 
-// const FormData = require('form-data')
-// const axios = require('axios').default
-
 const CreateJob = () => {
    const [labels, setLabels] = useState([])
-   // const [createJob, { loading, error, data }] = useMutation(CREATE_JOB)
    const [currentTotal, setCurrentTotal] = useState(0)
-   const [totalImages, setTotalImages] = useState(0)
    const [images, setImages] = useState([])
    const onChange = (imageList, addUpdateIndex) => {
-      // data for submit
-      // console.log(imageList, addUpdateIndex)
       setImages(imageList)
    }
-   const showError = () => {
-      toast.error('An error occured')
-      toast.clearWaitingQueue()
-   }
-   const updateImageCounter = (e) => {
-      const temp = document.querySelector('#testimageup').files.length
-      setTotalImages(temp)
-   }
+
    useEffect(() => {
       document.querySelector('#totalCredits').value = 0
    }, [])
 
-   // let myfiles
-   // const handlefiles = (e) => {
-   //    myfiles = e.target.files
-   // }
-
-   const createFormDataJob = async () => {}
-
+   
    const Calculate = (e) => {
       e.preventDefault()
       let currentCredits = document.querySelector('#credits').value
@@ -65,11 +43,20 @@ const CreateJob = () => {
             encType="multipart/form-data"
             onSubmit={async (e) => {
                e.preventDefault()
-               // let files = images.map((image) => image.file)[0]
-               
+               Calculate(e)
                const form = new FormData()
                //TODO Dont allow more partitions than images
+               if (
+                  images.length <=
+                  document.querySelector('#imgPerSection').value
+               ) {
+                  toast.error(
+                     'Invalid number of Partitions, make sure partitions does not exceed images'
+                  )
+                  return
+               }
                //! Begin Error checking
+
                if (document.querySelector('#title').value === '') {
                   toast.error('Please enter a valid job title')
                   // toast.clearWaitingQueue()
@@ -93,11 +80,6 @@ const CreateJob = () => {
                   // toast.clearWaitingQueue()
                   return
                }
-               //// if (dataForSubmit.images.length === 0) {
-               ////    toast.error('Please upload at least 1 image')
-               // //   // toast.clearWaitingQueue()
-               ////    return
-               //// }
                //! End Error Checking
 
                form.append(
@@ -106,7 +88,6 @@ const CreateJob = () => {
                      query: 'mutation ($files: [Upload], $title: String!, $description: String!, $credits: Int!, $num_partitions: Int!, $labels: [String]!){\n  createJob (files: $files, title: $title, description: $description, credits: $credits, num_partitions: $num_partitions, labels: $labels){\n    job_id\n  }\n}',
                      variables: {
                         files: [],
-                        // title: 'faisal test',
                         title: document.querySelector('#title').value,
                         description:
                            document.querySelector('#description').value,
@@ -130,67 +111,25 @@ const CreateJob = () => {
                for (let i = 0; i < images.length; i++) {
                   form.append(i.toString(), images[i].file)
                }
-
-               // console.log(form)
-               let res = axios.post(
-                  'https://data-labelling-server.herokuapp.com/graphql',
-                  form,
-                  {
-                     headers: {
-                        Authorization: 'Bearer ' + Cookies.get('jwt') || null
+               let res = axios
+                  .post(
+                     'https://data-labelling-server.herokuapp.com/graphql',
+                     form,
+                     {
+                        headers: {
+                           Authorization: 'Bearer ' + Cookies.get('jwt') || null
+                        }
                      }
-                  }
-               )
-
-               console.log(res)
-
-               // console.log(e.target.files)
-               // const dataForSubmit = {
-               //    jobTitle: document.querySelector('#title').value,
-               //    jobDescription: document.querySelector('#description').value,
-               //    labels: labels.map((label) => label.label),
-               //    // images: document.querySelector('#testimageup').files,
-               //    images: images.map((image) => image.file),
-               //    totalCreditsCost: currentTotal,
-               //    // numLabellers: parseInt(
-               //    //    document.querySelector('#numLabellers').value
-               //    // ),
-               //    numPartitions: parseInt(
-               //       document.querySelector('#imgPerSection').value
-               //    )
-               // }
-               // // console.log(dataForSubmit.images)
-               // if (dataForSubmit.jobTitle === '') {
-               //    toast.error('Please enter a valid job title')
-               //    // toast.clearWaitingQueue()
-               //    return
-               // }
-               // if (dataForSubmit.jobDescription === '') {
-               //    toast.error('Please enter a valid job description')
-               //    // toast.clearWaitingQueue()
-               //    return
-               // }
-               // if (dataForSubmit.labels.length === 0) {
-               //    toast.error('Please enter at least one label')
-               //    // toast.clearWaitingQueue()
-               //    return
-               // }
-               // if (dataForSubmit.images.length === 0) {
-               //    toast.error('Please upload at least 1 image')
-               //    // toast.clearWaitingQueue()
-               //    return
-               // }
-               // if (dataForSubmit.labels.some) console.log(dataForSubmit)
-               // await createJob({
-               //    variables: {
-               //       title: dataForSubmit.jobTitle,
-               //       description: dataForSubmit.jobDescription,
-               //       credits: dataForSubmit.totalCreditsCost,
-               //       labels: dataForSubmit.labels,
-               //       num_partitions: dataForSubmit.numPartitions,
-               //       files: dataForSubmit.images[0]
-               //    }
-               // }).catch((error) => showError())
+                  )
+                  .then(() => {
+                     
+                     toast.success('Your Job was successfully created')
+                     toast.clearWaitingQueue()
+                  })
+                  .catch(() => {
+                     toast.error('Failed to Create Your job')
+                     toast.clearWaitingQueue()
+                  })
             }}
          >
             <div className="createJob_mainForm">
@@ -308,22 +247,7 @@ const CreateJob = () => {
                </div>
                <div className="createJob_imageSection">
                   <h3>Please upload your images here:</h3>
-                  {/* <input
-                     id="testimageup"
-                     type="file"
-                     multiple
-                     onChange={updateImageCounter}
-                  />
-                  <label
-                     htmlFor="testimageup"
-                     className="btn-hover"
-                     id="labelImageUp"
-                     onChange={updateImageCounter}
-                     style={{ padding: '1rem' }}
-                  >
-                     Upload Images
-                  </label>
-                  <h2>Total Images : {totalImages}</h2> */}
+                  
                   <ImageUploading
                      multiple
                      value={images}
@@ -341,35 +265,38 @@ const CreateJob = () => {
                      }) => (
                         // write your building UI
                         <div className="upload__image-wrapper">
-                           <button
-                              className="btn-hover"
-                              style={isDragging ? { color: 'red' } : null}
-                              onClick={(e) => {
-                                 e.preventDefault()
-                                 onImageUpload()
-                              }}
-                              {...dragProps}
-                           >
-                              Click or Drop here
-                           </button>
-                           &nbsp;
-                           <button
-                              className="btn-hover"
-                              onClick={(e) => {
-                                 onImageRemoveAll()
-                                 e.preventDefault()
-                              }}
-                           >
-                              Remove all images
-                           </button>
-                           <h2>Total Images : {imageList.length}</h2>
+                           <div className="upload__button-section">
+                              <button
+                                 className="btn-hover"
+                                 style={isDragging ? { color: 'red' } : null}
+                                 onClick={(e) => {
+                                    e.preventDefault()
+                                    onImageUpload()
+                                 }}
+                                 {...dragProps}
+                              >
+                                 Click or Drop here
+                              </button>
+                              &nbsp;
+                              <button
+                                 className="btn-hover"
+                                 onClick={(e) => {
+                                    onImageRemoveAll()
+                                    e.preventDefault()
+                                 }}
+                              >
+                                 Remove all images
+                              </button>
+                           </div>
+
+                           <h3 style={{textAlign:"center"}}>Total Images : {imageList.length}</h3>
                            <div className="createJob_imagePrev">
                               {imageList.map((image, index) => (
                                  <div key={index} className="image-item">
                                     <img
                                        src={image.data_url}
                                        alt=""
-                                       width="200"
+                                       width="100"
                                     />
                                     {/* <div className="image-item__btn-wrapper">
                                        <button
@@ -406,21 +333,13 @@ const CreateJob = () => {
                      id="credits"
                      label="Credits"
                      type="number"
-                     // onChange={Calculate}
                      variant="outlined"
                   />
-                  {/* <TextField
-                     id="numLabellers"
-                     label="Number of Labellers"
-                     type="number"
-                     // onChange={Calculate}
-                     variant="outlined"
-                  /> */}
+
                   <TextField
                      id="imgPerSection"
                      label="Number of Partitions"
                      type="number"
-                     // onChange={Calculate}
                      variant="outlined"
                   />
                   <h2 id="totalCredits">Total: {currentTotal}</h2>
