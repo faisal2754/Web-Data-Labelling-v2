5db8d8ec22f4b89b254faee75772c5ea1fb62443
Ioanni-Balassis <1848800@students.wits.ac.za>
2021-10-12 11:16:02 +0200

    Completed Jobs Display Working
    Must still fix images

--- src/Pages/Dashboard/CompletedJobs.jsx
+++ src/Pages/Dashboard/CompletedJobs.jsx
@@ -39,8 +39,8 @@ function CompletedJobs() {
                   <div className="completeJobs__modal">
                      <Modal
                         id={job.job_id}
-                        src={job.preview_images[0]}
-                        // src="../images/purple_gradient.jpg"
+                        // src={job.preview_images[0]}
+                        src="../images/completed_job.png"
                         text={job.description}
                         credits={job.credits}
                         uploader={job.job_owner.username}
@@ -85,8 +85,8 @@ function CompletedJobs() {
                         >
                            <CardItem
                               // id={job.job_id.concat('card')}
-                              src={job.preview_images[0]}
-                              // src="../images/purple_gradient.jpg"
+                              // src={job.preview_images[0]}
+                              src="../images/completed_job.png"
                               text={job.description}
                               credits={job.credits}
                            />
