2416eb7799a6b15ff06e2eb5980c11b1b3855d56
Ioanni-Balassis <1848800@students.wits.ac.za>
2021-10-20 11:49:41 +0200

    Added 3 jobs to home carousel
    Still need to fix:
    modal
    spacing
    job order (3 most recent)

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,37 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CardItem from './CardItem'
 import '../Styles/Cards.css'
+import { useQuery } from '@apollo/client'
+import { GET_JOBS } from '../graphql/queries'
+import Modal from '../Components/Modal'
+import ReactLoading from 'react-loading'
+import { toast } from 'react-toastify'
 
 function Cards() {
+   let jobs = []
+   const { loading, error, data } = useQuery(GET_JOBS, {
+      fetchPolicy: 'cache-and-network'
+   })
+   if (data) {
+      jobs = data.viewJobs.slice(data.viewJobs.length - 3, data.viewJobs.length)
+   }
+   if (error) {
+      toast.error('There was an error fetching the jobs')
+   }
+
+   const [showModal, setShowModal] = useState(false)
+   const openModal = (currentId) => {
+      setShowModal((prev) => !prev)
+      for (let i = 0; i < jobs.length; i++) {
+         if (jobs[i].job_id !== currentId) {
+            document.getElementById(jobs[i].job_id).style.display = 'none'
+         } else {
+            document.getElementById(jobs[i].job_id).style.display = 'block'
+         }
+      }
+   }
+
    return (
       <div className="cards">
+         {jobs.map((job) => {
+            return (
+               <div className="viewJob__modal">
+                  <Modal
+                     id={job.job_id}
+                     src={job.preview_images[0]}
+                     text={job.description}
+                     credits={job.credits}
+                     uploader={job.job_owner.username}
+                     title={job.title}
+                     showModal={showModal}
+                     setShowModal={setShowModal}
+                     buttonLabel={'Accept Job'}
+                     destination="/dashboard/accepted-jobs"
+                     acceptable={true}
+                  />
+               </div>
+            )
+         })}
          <h2>Check Out Some of our Labelling Jobs</h2>
          <div className="cards__container">
             <div className="cards__wrapper">
-               <ul className="cards__items">
-                  
-                  <CardItem
-                     src="./images/purple_gradient.jpg"
-                     text="Here is a Labelling Job"
-                     credits="5000 credits"
-                     path="/"
+               {loading ? (
+                  <ReactLoading
+                     type={'spin'}
+                     height={'10%'}
+                     color={'#000000'}
+                     width={'10%'}
+                     className="acceptedJob__loadingSpin"
                   />
-
-                  <CardItem
-                     src="./images/purple_gradient.jpg"
-                     text="Here is another Labelling Job"
-                     credits="2500 credits"
-                     path="/"
-                  />
-
-                  <CardItem
-                     src="./images/purple_gradient.jpg"
-                     text="Here is a third Labelling Job"
-                     credits="1 credit"
-                     path="/"
-                  />
-
-               </ul>
+               ) : (
+                  <ul className="cards__items">
+                     {jobs.map((job) => {
+                        return (
+                           <div
+                              className="cards__itemsDisplay"
+                              onClick={(e) => openModal(job.job_id)}
+                           >
+                              <CardItem
+                                 src={job.preview_images[0]}
+                                 text={job.title}
+                                 credits={job.credits}
+                              />
+                           </div>
+                        )
+                     })}
+                  </ul>
+               )}
             </div>
          </div>
       </div>
