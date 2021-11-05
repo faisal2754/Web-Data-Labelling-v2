c7085fe7152721b041c88e192918051d77dcca7a
Ghulame Mujtaba Arbi <mujtaba.rbee091@gmail.com>
2021-11-05 11:31:46 +0200

    fixed redirection links of cards

--- src/Components/FindJobCards.jsx
+++ src/Components/FindJobCards.jsx
@@ -12,13 +12,13 @@ function PostJobCards() {
                      src="./images/HowTo/login2.gif"
                      text="In order to use our platform for finding a job, it is advisable for you to log in to gain full functionality of our platform but it is not required to view available jobs. If you haven't already, you can create an account here."
                      credits="Step 1"
-                     path="/how-to=find"
+                     path="/how-to-find"
                   />
                   <CardItem
                      src="./images/HowTo/login5.gif"
                      text="Navigating to the page will allow you to see all available data labelling jobs. You can also sort the jobs based on different filters. Select a data labelling job which you would like to work on. You can either accept the job or change your mind and hit cancel and unselect the job."
                      credits="Step 2"
-                     path="/how-to=find"
+                     path="/how-to-find"
                   />
                </ul>
             </div>
@@ -31,13 +31,13 @@ function PostJobCards() {
                      src="./images/HowTo/login.gif"
                      text="You can select accept job to work on the job you have picked out. If you have not already signed in, you will be prompted to. Once you are signed in you will be taken to your dashboard where you can view the selected job under 'Accepted Jobs' and work on it."
                      credits="Step 3"
-                     path="/how-to=find"
+                     path="/how-to-find"
                   />
                   <CardItem
                      src="./images/HowTo/find.gif"
                      text="Select the accepted job in your dashbaord to start the data labelling process. On the labelling page, select a label for each image. Once the labelling is complete, you will receive a success message to inform you that the labelling job is completed."
                      credits="Step 4"
-                     path="/how-to=find"
+                     path="/how-to-find"
                   />
                </ul>
             </div>
